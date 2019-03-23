afc5e0d8aae7a71a7f1123bf5189825d54f26449
smartmanpsv <petersonvalmyy@gmail.com>
2019-03-23 09:48:03 -0700

    Commit initial
    Signed-off-by: smartmanpsv <petersonvalmyy@gmail.com>

---
+++ app/src/main/java/com/example/smart/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.smart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
